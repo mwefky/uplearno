cb40a40f8c946873baf1c109ead1ab0bd153aa57
wefky <mina@dozzan.com>
2021-02-08 16:52:38 +0200

    Initial Commit

---
+++ Uplearno/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uplearno
+//
+//  Created by mina wefky on 2/8/21.
+//  Copyright © 2021 mina wefky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
